a2dd5f44160a7173bb24ebf2d1e60e33ba5d8425
Sprint-Development <117239384+Sprint-Development@users.noreply.github.com>
2022-11-02 01:42:35 +0100

    Update Sprint-Firewall.sh

--- Sprint-Firewall.sh
+++ Sprint-Firewall.sh
@@ -53,18 +53,3 @@ iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP
-
-#===>#[Minecraft Proxy]#<===#
-iptables -A INPUT -p tcp -m tcp --dport 25565 --tcp-option 8 --tcp-flags FIN,SYN,RST,ACK SYN -j REJECT --reject-with icmp-port-unreachable
-iptables -A INPUT -p tcp -m tcp --dport 25565 -m state --state RELATED.ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 25565 --tcp-flags FIN,SYN,RST,ACK SYN -m connlimit --connlimit-above 150 --connlimit-mask 32 --connlimit-saddr -j DROP
-iptables -A INPUT -p tcp -m tcp --dport 25565 --tcp-flags FIN,SYN,RST,ACK SYN -m connlimit --connlimit-above 10 --connlimit-mask 32 --connlimit-saddr -j DROP
-iptables -A INPUT -p tcp -m tcp --sport 123 -j REJECT --reject-with icmp-port-unreachable
-iptables -A INPUT -p tcp -m tcp --sport 389 -j REJECT --reject-with icmp-port-unreachable
-
-iptables -A INPUT -p tcp -m state --state NEW -m limit --limit 50/second --limit-burst 50 -j ACCEPT
-iptables -A INPUT -p tcp -m state --state NEW -j DROP
-iptables -A INPUT -p tcp -m state --state RELATED.ESTABLISHED -j ACCEPT
-
-iptables -A INPUT -p tcp -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT
-iptables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -k ACCEPT
